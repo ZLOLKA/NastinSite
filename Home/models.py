7dcb7750ec6b945cd60c68eb08afeb77d0b19126
ZLOLKA <jaroslavderkach@gmail.com>
2019-02-04 19:48:02 +0300

    Change models

--- Home/models.py
+++ Home/models.py
@@ -15,7 +15,7 @@ class Maker(models.Model):
 
     name = models.CharField(max_length=100)
     slug = models.SlugField(blank=True)
-    experience = models.DecimalField(blank=True)
+    experience = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
 
     def __str__(self):
         return self.name
