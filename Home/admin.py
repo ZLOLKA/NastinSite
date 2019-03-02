e83b805558aeb3233b96a33e139246acb98436ad
ZLOLKA <jaroslavderkach@gmail.com>
2019-03-02 11:44:53 +0300

    Register Maker

--- Home/admin.py
+++ Home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Home.models import Category, Product
+from Home.models import Category, Product, Maker
 
 
 class TitlePrepopulated(admin.ModelAdmin):
@@ -9,3 +9,4 @@ class TitlePrepopulated(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Category, TitlePrepopulated)
 admin.site.register(Product, TitlePrepopulated)
+admin.site.register(Maker, TitlePrepopulated)
