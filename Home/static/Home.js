aec92e003a4a837fad2978ad311b1aecab5100ab
ZLOLKA <jaroslavderkach@gmail.com>
2019-03-05 00:07:43 +0300

    Normalization menu moving

--- Home/static/Home.js
+++ Home/static/Home.js
@@ -1,18 +1,5 @@
-function showHide(thisObj, obj)
-{
-    console.log(obj.style.left);
-    var startPosition = (obj.style.left).toString().slice(0, -2);
-
-    if (obj.style.left == "-390px")
-    {
-        alert(obj);
-        thisObj.style.transform = 'rotate(180deg)'
-    }
-    else
-    {
-        //document.getElementsByClassName('Menu')[0].style.left = '-390px';
-        thisObj.style.transform = 'rotate(0deg)'
-    }
+function showHide() {
+    $(".Menu").toggleClass('active');
 }
 function aOver(obj) {
     obj.style.textDecoration = 'underline';
